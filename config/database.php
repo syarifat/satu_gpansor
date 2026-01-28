7d81453c5db84362171d00ecbaf5ee0b7a2d6b02
syarifat <syarifahsanit@gmail.com>
2026-01-28 19:07:16 +0700

    CA TIDB

--- config/database.php
+++ config/database.php
@@ -59,7 +59,10 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA') 
+                                            ? storage_path('app/' . env('MYSQL_ATTR_SSL_CA')) 
+                                            : null,
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ]) : [],
         ],
 
